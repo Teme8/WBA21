f12d171654c09320a945e2b45297e07bca277862
teemutir <teemu.tirkkonen@metropolia.fi>
2021-01-11 12:18:44 +0200

    Test2

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Testing</Text>
+      <Text>Testing2</Text>
       <StatusBar style="auto" />
     </View>
   );
