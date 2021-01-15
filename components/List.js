afe01fcf5fe6752c58b69cdf226d007dce71f328
teemutir <teemu.tirkkonen@metropolia.fi>
2021-01-15 09:57:35 +0200

    Done

--- components/List.js
+++ components/List.js
@@ -1,58 +1,53 @@
-import React from "react";
-import { FlatList, StyleSheet, Text, View, ImageBackground, StatusBar} from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  StatusBar,
+} from "react-native";
 import ListItem from "./ListItem";
 
-const mediaArray = [
-  {
-    key: "0",
-    title: "Miu",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.",
-    thumbnails: {
-      w160: "http://placekitten.com/160/161",
-    },
-    filename: "http://placekitten.com/2048/1920",
-  },
-  {
-    key: "1",
-    title: "Mau",
-    description:
-      "Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ",
-    thumbnails: {
-      w160: "http://placekitten.com/160/164",
-    },
-    filename: "http://placekitten.com/2041/1922",
-  },
-  {
-    key: "2",
-    title: "Miau",
-    description:
-      "Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ",
-    thumbnails: {
-      w160: "http://placekitten.com/160/167",
-    },
-    filename: "http://placekitten.com/2039/1920",
-  },
-];
+const url =
+  "https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json";
 
 const List = () => {
+  const [mediaArray, setMediaArray] = useState([]);
+
+  const loadMedia = async () => {
+    try{
+      const response = await fetch(url);
+      const json = await response.json();
+      console.log(json);
+      setMediaArray(json);
+    } catch (error) {
+      console.error('loadMedia error', error);
+    }
+  };
+
+  useEffect(() => {
+    loadMedia();
+  }, []);
+
   return (
-    <View style={styles.container} >
-    <StatusBar backgroundColor="black" barStyle="light-content" />
-    <View style={styles.header} >
-    <ImageBackground
-      source={require('../assets/cats.jpg')}
-      style={styles.bgimage}
-      imageStyle={{borderRadius: 0}}
-      ></ImageBackground>
-      <Text style={styles.hello}>Cool cats!</Text>
+    <View style={styles.container}>
+      <StatusBar backgroundColor="black" barStyle="light-content" />
+      <View style={styles.header}>
+        <ImageBackground
+          source={require("../assets/cats.jpg")}
+          style={styles.bgimage}
+          imageStyle={{ borderRadius: 0 }}
+        ></ImageBackground>
+        <Text style={styles.hello}>Cool cats!</Text>
       </View>
       <View style={styles.flat}>
-    <FlatList
-      data={mediaArray}
-      renderItem={({ item }) => <ListItem singleMedia={item}/>}
-    />
-    </View>
+        <FlatList
+          data={mediaArray}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => <ListItem singleMedia={item} />}
+        />
+      </View>
     </View>
   );
 };
@@ -69,8 +64,8 @@ const styles = StyleSheet.create({
   },
   bgimage: {
     flex: 1,
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
   },
   header: {
     flex: 1,
@@ -78,13 +73,13 @@ const styles = StyleSheet.create({
     backgroundColor: "green",
     marginBottom: 20,
     borderWidth: 1,
-    borderColor: 'black',
+    borderColor: "black",
   },
   hello: {
-    position: 'absolute',
+    position: "absolute",
     top: 20,
     left: 40,
-    color: 'blue',
+    color: "blue",
   },
   flat: {
     flex: 1.5,
